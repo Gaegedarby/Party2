e6db61c8f63dc8f6ff5cb0015c2c42580a8e5266
Gaegedarby <Manirajabi2006@gmail.com>
2024-11-14 10:06:09 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function rsvp() {
   alert("Thanks for your RSVP!");
 }
+function toggleMusic() {
+  const music = document.getElementById('background-music');
+  if (music.paused) {
+    music.play();
+  } else {
+    music.pause();
+  }
+}
